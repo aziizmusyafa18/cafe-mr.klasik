621834a70ae5be1c015ec84ae10a4bc2527f3907
aziizmusyafa18 <luthfimusyafa18@gmail.com>
2025-05-27 12:19:25 +0700

    hapus cart,update responsive

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 //toggle class active
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 
-document.querySelector('#hamburger-menu').onclick = () => {
-    navbarNav.classList.toggle('active');
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
-const hamburger = document.querySelector('#hamburger-menu');
+const searchFrom = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+document.querySelector("#search-btn").onclick = (e) => {
+  searchFrom.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
 
-document.addEventListener('click', function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active')
-    }
-    
+const hamburger = document.querySelector("#hamburger-menu");
+const searchBtn = document.querySelector("#search-btn");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!searchBtn.contains(e.target) && !searchFrom.contains(e.target)) {
+    searchFrom.classList.remove("active");
+  }
 });
